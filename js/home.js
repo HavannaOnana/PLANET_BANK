7a8b2e157c83c83a2948461630f9d26ed7fcb9cd
HavannaOnana <138626351+HavannaOnana@users.noreply.github.com>
2024-06-02 15:23:14 +0200

    3% done

--- js/home.js
+++ js/home.js
@@ -10,15 +10,20 @@ let ALinks = document.querySelectorAll("a");
 // selecting the image 
 let Logo = document.querySelector(".logo");
 
+// made the darkmode false so i can switch it 
 let IsDarkMode = false;
+
+// selecting an image in the html
+let PlanetBankImage= document.querySelector("#mainbody_img");
 
 // add an event listener to it 
 Darkmode.addEventListener("click" , function(){
   // this toggles the value of the boolean variable
   IsDarkMode = !IsDarkMode;
-  
+
   if(IsDarkMode){
     Logo.setAttribute("src", "/images/dark mode-Photoroom.png-Photoroom.png");
+    PlanetBankImage.setAttribute("src","/images/dark mode-Photoroom.png-Photoroom.png")
     HtmlBody.classList.add("darkmode");
     ALinks.forEach(element => {
         element.style.color = "white";
@@ -27,6 +32,7 @@ Darkmode.addEventListener("click" , function(){
   else{
     HtmlBody.classList.remove("darkmode");
     Logo.setAttribute("src", "/images/1-Photoroom.png-Photoroom.png");
+    PlanetBankImage.setAttribute("src", "/images/1-Photoroom.png-Photoroom.png")
     ALinks.forEach(element => {
         element.style.color = "black";
     });
