5da98ab1bcea165253439e7528c615133941e13d
HavannaOnana <138626351+HavannaOnana@users.noreply.github.com>
2024-06-13 23:57:47 +0200

    busy with dashboard first row

--- js/dashboard.js
+++ js/dashboard.js
@@ -13,6 +13,9 @@ let ALinks = document.querySelectorAll("a");
 // made the darkmode false so i can switch it 
 let IsDarkMode = false;
 
+//selecting savings account node
+let SavingsAccount = document.querySelector(".saving_account");
+
 //adding event listener 
 Darkmode.addEventListener("click",function(){
     IsDarkMode = !IsDarkMode
@@ -23,6 +26,7 @@ Darkmode.addEventListener("click",function(){
         ALinks.forEach(element => {
             element.style.color = "white";
         });
+        SavingsAccount.style.color ="black"
     }
     else{
         HtmlBody.classList.remove("darkmode");
